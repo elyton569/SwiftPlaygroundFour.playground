a805879c94b8a6d0bfa736b915096617c21cc290
CM Student <cmstudent@uhwo-acm-10.uhwo.hawaii.edu>
2020-02-25 10:23:50 -1000

    Completed Introduction to Loops

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//part 4
+for number in 1...5{
+//created number one to five
+    print(number)
+//created number one and five to print
+}
+//start graphically display the iteration of sum += number
+//part 5
+// varaiable sum will only equal zero
+var sum = 0
+//created number one to five
+for number in 1...5{
+   sum += number
+   print(sum)
+}
+//created new loops in loops
+//Part 6 While Loops
+//created variable I equal one
+var i = 1
+while i <= 10
+//created bracket to the left
+//finished Introduction to Loops
+{
+    
+    i = i + 1
+       print(i)
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# SwiftPlaygroundFour.playground
